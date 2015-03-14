2a636cd284cdf340ec6ae9059431bbccadf89428
sijayaraman <sijayaraman@paypal.com>
2015-03-14 22:46:09 +0530

    Spring init and destroy methods

---
+++ SpringBasicBeanConfiguration/Spring04-InitAndDestroyMethods/src/main/java/com/spring/bl/Employee.java
@@ -0,0 +1,57 @@
+package com.spring.bl;
+
+public class Employee {
+
+	private int id;
+	private String username;
+	private int age;
+	private String location;
+
+	public void init(){
+		System.out.println("Init method called after bean initialization");
+	}
+	
+	public void destroy(){
+		System.out.println("destroy method called after bean destroyed");
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String name) {
+		this.username = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + username + ", age=" + age
+				+ ", location=" + location + "]";
+	}
+	
+
+}
